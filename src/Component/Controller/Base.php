41bbf8c73370c3dc326d106f959c2aed260998cd
haoyankai <haoyankai@hao360cn02v.ops.corp.qihoo.net>
2015-07-23 10:45:25 +0800

    修改controller

--- src/Component/Controller/Base.php
+++ src/Component/Controller/Base.php
@@ -1,35 +1,178 @@
 <?php
+/**
+* @file Base.php
+* @brief controller base class
+* @author haoyankai
+* @version 1.0
+* @date 2015-07-20
+ */
+
 namespace Vine\Component\Controller;
 
-/**
-    * This is controller base
- */
 abstract class Base implements \Vine\Component\Controller\ControllerInterface
-{/*{{{*/
-    protected $request = null;
-    protected $response = null;
+{
+    /**  
+     * @var string current module name.
+     */
+    private $moduleName;
 
+    /**  
+     * @var string current controller name.
+     */
+    private $controllerName;
+
+    /**  
+     * @var current action name.
+     */
+    private $actionName;
+
+    /**  
+     * @var \Vine\Component\View\ViewInterface the view used to render template.
+     */
+    protected $view;
+
+    /**  
+     * @var boolean whether render view or not.
+     */
+    protected $isViewRender;
+
+    /**  
+     * @var \Vine\Component\Http\RequestInterface request object.
+     */
+    protected $request;
+
+    /**  
+     * @var \Vine\Component\Http\ResponseInterface response object.
+     */
+    protected $response;
+
+    /**  
+     * {@inheritdoc}
+     */
     public function __construct($moduleName, $controllerName, $actionName)
-    {/*{{{*/
-    }/*}}}*/
+    {
+        if (empty($moduleName)) {
+            $moduleName = '';
+        }
+        $this->moduleName = $moduleName;
+        $this->controllerName = $controllerName;
+        $this->actionName = $actionName;
+    }
+
+    /**  
+     * module name getter.
+     * @return string module name.
+     */
+    public function getModuleName()
+    {
+        return $this->moduleName;
+    }
+
+    /**  
+     * controller name getter.
+     * @return string controller name.
+     */
+    public function getControllerName()
+    {
+        return $this->controllerName;
+    }
+
+    /**  
+     * action name getter.
+     * @return string action name.
+     */
+    public function getActionName()
+    {
+        return $this->actionName;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
     public function setView(\Vine\Component\View\ViewInterface $view)
-    {/*{{{*/
-    }/*}}}*/
-    public function setRequest(\Vine\Component\Http\RequestInterface $request)
-    {/*{{{*/
-        $this->request = $request;
-    }/*}}}*/
-    public function setResponse(\Vine\Component\Http\ResponseInterface $response)
-    {/*{{{*/
-        $this->response = $response;
-    }/*}}}*/
+    {
+        $this->view = $view;
+    }
+
+    /**
+     * set whether to render view or not.
+     * true indicates the action will render view, false not.
+     * @param boolean $isRender whether to render view or not.
+     */
+    public function setViewRender($isRender)
+    {
+        $this->isViewRender = $isRender;
+    }
+
+    /**
+     * redirect to destination url.
+     * @see \Vine\Component\Http\Response::redirect() for detail information.
+     * @param string $url redirect destination.
+     */
+    public function redirect($url)
+    {   
+        $this->response->redirect($url);
+    }
+
+    /**
+     * render data into template and return generated html.
+     * @param string $tpl template name.
+     * If empty string is given, the template with action name will be rendered.
+     * @param boolean $withViewSuffix whether parameter $tpl with suffix or not.Default to be false.
+     * EXAMPLE:If $tpl='index.php', $withViewSuffix should be true.
+     *         If $tpl='index', $withViewSuffix should be false.
+     * @param array $data data to render into template.
+     * @return string|boolean generated html or false if $isViewRender set to false. 
+     */
+    public function render($tpl = '', $withViewSuffix = false, array $data = array())
+    {
+        if ($this->isViewRender === true) {
+            $tplFolder = $this->moduleName . '/' . $this->controllerName . '/';
+            if ($tpl != '') {
+                $tplPath = $tplFolder . $tpl;
+            } else {
+                $tplPath = $tplFolder . $this->getActionName();
+            }
+            return $this->view->render($tplPath, $withViewSuffix, $data);
+        }
+        return false;
+    }
+
+    /**
+     * render data into template and set generated html into response object.
+     * @param string $tpl template name.
+     * @param boolean $withViewSuffix.
+     * @param array $data data to render into template.
+     */
+    public function display($tpl = '', $withViewSuffix = false, array $data = array())
+    {
+        $content = $this->render($tpl, $withViewSuffix, $data);
+        if ($content !== false) {
+            $this->response->setBody($content);
+        }
+    }
+
+    /**  
+     * {@inheritdoc}
+     */
     public function autoRender()
-    {/*{{{*/
-    }/*}}}*/
-    public function beforeAction()
-    {/*{{{*/
-    }/*}}}*/
-    public function afterAction()
-    {/*{{{*/
-    }/*}}}*/
-}/*}}}*/
+    {
+        $this->display();
+    }
+
+    /**  
+     * {@inheritdoc}
+     */
+    protected function beforeAction()
+    {
+        return true;
+    }
+
+    /**  
+     * {@inheritdoc}
+     */
+    protected function afterAction()
+    {
+    }
+
+}
